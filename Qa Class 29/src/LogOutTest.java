8a9a898f6bb2e8476683c4bc0a9c7bc5f82f5ec5
Som <somaiah1988@gmail.com>
2025-10-18 06:34:57 +0530

    adding files

--- Qa Class 29/src/LogOutTest.java
+++ Qa Class 29/src/LogOutTest.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -6,13 +8,14 @@ public class LogOutTest {
 	public void logout() throws InterruptedException {
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));//maximum timeout
 		driver.get("https://www.saucedemo.com");
 
 		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
 	    driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce"); 
 	    driver.findElement(By.xpath("//input[@id='login-button']")).click();
 	   driver.findElement(By.xpath("//button[@id='react-burger-menu-btn']")).click();
-	    Thread.sleep(2000);
+	  
 	   driver.findElement(By.xpath("//a[@id='logout_sidebar_link']")).click();
 
 	}
