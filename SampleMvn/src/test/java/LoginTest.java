0eb6d2f3d6f4e3af6eeb324dcf7b46f5bfdd07b9
Som <somaiah1988@gmail.com>
2025-10-28 07:16:09 +0530

    adding files

---
+++ SampleMvn/src/test/java/LoginTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void validlogin() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com");
+		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+	    driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce"); 
+	    driver.findElement(By.xpath("//input[@id='login-button']")).click();
+	    driver.quit();
+	}
+		
+	@Test
+public void invalidlogin() {
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.saucedemo.com");
+	driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+    driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce1"); 
+    driver.findElement(By.xpath("//input[@id='login-button']")).click();
+    driver.quit();
+}
+	}
